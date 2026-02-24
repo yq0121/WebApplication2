f8ad3f9fd971dca87cd2b5ddf750a86272cec5a4
Yap Yong Qi <yq.yap@i-Ruma.com>
2026-02-24 16:28:45 +0800

    Add New things

--- WebApplication2/Models/Order.cs
+++ WebApplication2/Models/Order.cs
@@ -12,12 +12,33 @@ namespace WebApplication2.Models
         Confirmed,
         Cancelled
     }
+
+    public enum PaymentMethod
+    {
+        None,
+        Cash,
+        CreditCard,
+        eWallet
+    }
+
+    public enum PaymentStatus
+    {
+        Pending,
+        Paid,
+        Failed
+    }
+
     public class Order
     {
         public int OrderId { get; set; }
         public DateTime OrderDate { get; set; }
         public OrderStatus Status { get; set; }
         public List<OrderItem> Items { get; set; }
+
+        // --- Payment info ---
+        public PaymentMethod paymentMethod { get; set; } = PaymentMethod.None;
+        public PaymentStatus paymentStatus { get; set; } = PaymentStatus.Pending;
+        public string TransactionRef { get; set; }
         public decimal Total
         {
             get 
